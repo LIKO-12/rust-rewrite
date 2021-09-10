85b2b7aee924580d51842461b726d1501ad75c2d
abledbody <drewisakid@gmail.com>
2021-09-09 21:00:35 -0400

    replace add_method! macro with add_method function

--- src/peripheral.rs
+++ src/peripheral.rs
@@ -1,3 +1,4 @@
+use v8::*;
 
 pub trait Peripheral {
     /// Must return the peripheral's type.
@@ -6,4 +7,11 @@ pub trait Peripheral {
 
     /// Creates an ObjectTemplate within the provided scope, which would contain the peripheral's JS api.
     fn js_api<'a>(&self, scope: &mut v8::HandleScope<'a, ()>) -> v8::Local<'a, v8::ObjectTemplate>;
+}
+
+pub fn add_method(template: ObjectTemplate, scope: HandleScope, name: impl ToString, callback: impl MapFnTo<FunctionCallback>) {
+	template.set(
+		String::new(scope, name).unwrap().into(),
+		FunctionTemplate::new(scope, callback).into(),
+	);
 }
